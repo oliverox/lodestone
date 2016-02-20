d6ad3cb1222f1a6f9cb1dda3fe807a506bd7b647
Oliver Oxenham <oliver.oxenham@gmail.com>
2016-02-20 00:00:11 -0800

    using better-scripts

--- src/app.js
+++ src/app.js
@@ -1,21 +1,16 @@
 /*eslint no-console: 0*/
 if (module.hot) {
+  console.debug('module.hot === true');
   module.hot.accept();
 }
-import mainStyles from './styles/main.scss';
+import './styles/main.scss';
 import styles from './styles/app.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { SimpleButton } from 'components';
 
-console.log('SimpleButton:', SimpleButton, mainStyles, styles);
-// var testNode = document.createElement('div');
-// var textNode = document.createTextNode('This is Lodestone!');
-// testNode.appendChild(textNode);
-// testNode.className = styles.test;
-// document.getElementById('app').appendChild(testNode);
-// document.getElementsByTagName('html')[0].style.backgroundColor = "pink";
+console.log('SimpleButton:', SimpleButton, styles);
 const dest = document.getElementById('app');
 const onButtonClick = () => {
   console.log('button was clicked! hmm');
